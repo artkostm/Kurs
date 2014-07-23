1a1a5388a7f5e973c67ac9394c0da46bd00d8c90
artkostm <artkostm@gmail.com>
2014-07-23 19:08:53 +0300

    Update Log4jInit.java

--- src/java/by/bsuir/chujko/Log4jInit.java
+++ src/java/by/bsuir/chujko/Log4jInit.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- * Simple test on GitHub :)
+ * Simple test on GitHub :) Hello from GitHub!
  */
 
 package by.bsuir.chujko;
